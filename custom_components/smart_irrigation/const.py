7138c6770ae5f3c5e7cc6b5b9edd2256f142fb76
Jeroen ter Heerdt <jterh@microsoft.com>
2020-05-26 17:04:00 -0700

    updating labels

--- custom_components/smart_irrigation/const.py
+++ custom_components/smart_irrigation/const.py
@@ -3,7 +3,7 @@
 DOMAIN = "smart_irrigation"
 NAME = "Smart Irrigation"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.12"
+VERSION = "0.0.13"
 
 ISSUE_URL = "https://github.com/jeroenterheerdt/HASmartIrrigation/issues"
 
