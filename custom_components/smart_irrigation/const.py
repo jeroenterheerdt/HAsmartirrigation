db15b1b3223df9deb69fd5c7b2327b1fc935d16b
Jeroen ter Heerdt <jterh@microsoft.com>
2022-12-09 12:26:52 -0800

    V0.0.78 - production release fixing #114

--- custom_components/smart_irrigation/const.py
+++ custom_components/smart_irrigation/const.py
@@ -3,7 +3,7 @@
 DOMAIN = "smart_irrigation"
 NAME = "Smart Irrigation"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.77-beta"
+VERSION = "0.0.78"
 
 ISSUE_URL = "https://github.com/jeroenterheerdt/HASmartIrrigation/issues"
 
