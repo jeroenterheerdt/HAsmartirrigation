b40d6c2017f68658c142bfc4d1edc351863f5b13
Mathijs Meulendijks <mathijs.meulendijks@gmail.com>
2023-06-10 09:18:54 +0000

    Initial testing setup

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the Smart Irrigation integration."""
---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+"""Fixtures for testing."""
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    yield
