699bea86dab15e368b5a3000f9164e8fa42ce5c9
Jeroen ter Heerdt <jterh@microsoft.com>
2023-06-19 13:08:54 -0700

    prepping for v0.0.79

--- custom_components/smart_irrigation/const.py
+++ custom_components/smart_irrigation/const.py
@@ -3,7 +3,7 @@
 DOMAIN = "smart_irrigation"
 NAME = "Smart Irrigation"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.78"
+VERSION = "0.0.79"
 
 ISSUE_URL = "https://github.com/jeroenterheerdt/HASmartIrrigation/issues"
 
