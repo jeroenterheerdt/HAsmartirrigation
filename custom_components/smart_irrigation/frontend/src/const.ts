9b3584d52c398ac908c3c4b7f8de4d6d418c493a
Jeroen ter Heerdt <jeroen_ter_heerdt@hotmail.com>
2023-09-02 04:09:21 +0000

    v2023.9.0-beta5 changing default auto update interval to 1 hour instead of 1 day

--- custom_components/smart_irrigation/frontend/src/const.ts
+++ custom_components/smart_irrigation/frontend/src/const.ts
@@ -1,4 +1,4 @@
-export const VERSION = "v2023.9.0-beta4";
+export const VERSION = "v2023.9.0-beta5";
 export const REPO = "https://github.com/jeroenterheerdt/HASmartIrrigation;";
 export const ISSUES_URL = REPO + "/issues";
 
