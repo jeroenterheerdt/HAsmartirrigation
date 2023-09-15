37c7518713676e27b59e807d52ff603cba0ff2d3
Jeroen ter Heerdt <jeroen_ter_heerdt@hotmail.com>
2023-09-15 21:54:09 +0000

    v2023.9.0-beta19 again tries to fix #271 and #259

--- custom_components/smart_irrigation/frontend/src/const.ts
+++ custom_components/smart_irrigation/frontend/src/const.ts
@@ -1,4 +1,4 @@
-export const VERSION = "v2023.9.0-beta20";
+export const VERSION = "v2023.9.0-beta19";
 export const REPO = "https://github.com/jeroenterheerdt/HASmartIrrigation;";
 export const ISSUES_URL = REPO + "/issues";
 
