89723132e4ea739fa0e3694386eb8c5d893b9ee5
Jeroen ter Heerdt <jterh@microsoft.com>
2025-04-28 17:04:55 +0000

    v2025.4.1 updating frontend packages

--- custom_components/smart_irrigation/frontend/dist/smart-irrigation.js
+++ custom_components/smart_irrigation/frontend/dist/smart-irrigation.js
@@ -1104,7 +1104,7 @@ i.version="2.30.1",n(Za),i.fn=us,i.min=Ja,i.max=Qa,i.now=ei,i.utc=m,i.unix=ds,i.
             .narrow=${this.narrow}
           ></ha-menu-button>
           <div class="main-title">${Zi("title",this.hass.language)}</div>
-          <div class="version">${"v2024.10.3"}</div>
+          <div class="version">${"v2025.4.0"}</div>
         </div>
 
         <ha-tabs
